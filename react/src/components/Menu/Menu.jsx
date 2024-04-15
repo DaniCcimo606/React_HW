50635804911cbda313def85fa254961e2da34728
DaniCcimo606 <bessmertnyj228@gmail.com>
2024-04-15 17:30:32 +0300

    Update homeWork_45

--- react/src/components/Menu/Menu.jsx
+++ react/src/components/Menu/Menu.jsx
@@ -10,8 +10,13 @@ const Menu = () => {
                 const res = await fetch(
                     'https://react-fast-pizza-api.onrender.com/api/menu'
                 );
+
                 const { data } = await res.json();
-                console.log(data);
+
+                if (!res.ok) {
+                    throw new Error('Failed to fetch!');
+                }
+
                 setPizzas(data);
             };
 
